f4373f683246a7aab2314c05c033f35a2874721d
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2026-01-19 03:35:33 +0530

    implemented auth layout and pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,55 +3,53 @@ import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="auth-layout">
-      <section className="auth-left-section scrollbar-hide-default">
-        <Link href="/" className="auth-logo max-w-max">
-          <Logo className="w-auto h-8" />
-        </Link>
+const AuthLayout = ({ children }: { children: React.ReactNode }) => (
+  <div className="auth-layout">
+    <section className="auth-left-section scrollbar-hide-default">
+      <Link href="/" className="auth-logo max-w-max">
+        <Logo className="w-auto h-8" />
+      </Link>
 
-        <div className="flex-1 pb-6 lg:pb-8">{children}</div>
-      </section>
+      <div className="flex-1 pb-6 lg:pb-8">{children}</div>
+    </section>
 
-      <section className="auth-right-section">
-        <div className="lg:mt-4 lg:mb-16 relative z-10">
-          <blockquote className="auth-blockquote">
-            PepTickr turned my watchlist into a winning list. The alerts are
-            spot-on, and I feel more confident making moves in the market
-          </blockquote>
+    <section className="auth-right-section">
+      <div className="lg:mt-4 lg:mb-16 relative z-10">
+        <blockquote className="auth-blockquote">
+          PepTickr turned my watchlist into a winning list. The alerts are
+          spot-on, and I feel more confident making moves in the market.
+        </blockquote>
 
-          <div className="flex justify-between items-center">
-            <div>
-              <cite className="auth-testimonial-author">&mdash; Ethan R.</cite>
-              <p className="text-gray-500 max-md:text-xs">Retail Investor</p>
-            </div>
+        <div className="flex justify-between items-center">
+          <div>
+            <cite className="auth-testimonial-author">&mdash; Ethan R.</cite>
+            <p className="text-gray-500 max-md:text-xs">Retail Investor</p>
+          </div>
 
-            <div className="flex items-center gap-0.5">
-              {[1, 2, 3, 4, 5].map((i) => (
-                <StarIcon
-                  key={i}
-                  size={20}
-                  strokeWidth="0"
-                  className="fill-gray-100"
-                />
-              ))}
-            </div>
+          <div className="flex items-center gap-0.5">
+            {[1, 2, 3, 4, 5].map((item) => (
+              <StarIcon
+                key={item}
+                size={20}
+                strokeWidth="0"
+                className="fill-yellow-400"
+              />
+            ))}
           </div>
         </div>
+      </div>
 
-        <div className="flex-1 relative">
-          <Image
-            src="/images/full-dashboard.png"
-            alt="Dashboard preview image"
-            width={1440}
-            height={1150}
-            className="auth-dashboard-preview absolute top-0"
-          />
-        </div>
-      </section>
-    </div>
-  );
-}
+      <div className="flex-1 relative">
+        <Image
+          src="/images/full-dashboard.png"
+          alt="Dashboard preview image"
+          width={1440}
+          height={1150}
+          className="auth-dashboard-preview absolute top-0"
+        />
+      </div>
+    </section>
+  </div>
+);
 
 export default AuthLayout;
--- components/form/InputField.tsx
+++ components/form/InputField.tsx
@@ -1,4 +1,6 @@
 import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
 
 function InputField({
   type = "text",
@@ -8,12 +10,29 @@ function InputField({
   disabled,
   placeholder,
   register,
-  validation,
+  validation = {},
   error,
 }: FormInputProps) {
   return (
     <div className="space-y-2">
-      <Label>Label</Label>
+      <Label htmlFor={name} className="form-label">
+        {label}
+      </Label>
+
+      <Input
+        type={type}
+        id={name}
+        value={value}
+        disabled={disabled}
+        placeholder={placeholder}
+        className={cn("form-input", {
+          "opacity-50 cursor-not-allowed": disabled,
+          "border-red-500 focus:border-red-500": error,
+        })}
+        {...register(name, validation)}
+      />
+
+      {error && <p className="text-red-500 text-sm">{error.message}</p>}
     </div>
   );
 }
